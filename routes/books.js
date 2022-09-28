6622929acf1b0d77ff8ad57c36935ffede5253ee
ShivaKanchi <shivakanchi111@gmail.com>
2022-09-28 22:05:53 +0530

    Book routes started

--- routes/books.js
+++ routes/books.js
@@ -1,5 +1,22 @@
 const express = require('express');
 
+const { books } = require('../data/books.json');
 const router = express.Router();
+
+
+/**
+ * Route : /books
+ * Method: GET
+ * Description: Get all books
+ * Access: Public
+ * Parameters: None
+ */
+router.get('/', (req, res) => {
+    return res.status(200).json({
+        success: true,
+        data: books
+    });
+})
+
 
 module.exports = router;
